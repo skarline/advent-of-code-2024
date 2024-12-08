44d51d38a524ea99c1cf16099afd17617678757e
Lihuen Molina <40340562+skarline@users.noreply.github.com>
2024-12-08 12:33:44 -0300

    Update runner script

--- main.ts
+++ main.ts
@@ -1,24 +1,36 @@
-for (let i = 1; i <= 25; i++) {
+async function runChallenge(day: number) {
   try {
-    const { part1, part2 } = await import(`./src/${i}.ts`);
-    const input = await Deno.readTextFile(`./src/${i}.txt`);
+    const { part1, part2 } = await import(`./src/${day}.ts`);
+    const input = await Deno.readTextFile(`./src/${day}.txt`);
 
-    console.log(`[Day ${i}]`);
+    console.log(`[Day ${day}]`);
 
-    if (part1) {
-      performance.mark("A");
-      const answer = part1(input);
-      const { duration } = performance.measure("A", "A");
-      console.log("Part 1:", answer, `(${duration.toFixed(2)}ms)`);
-    }
-    if (part2) {
-      performance.mark("B");
-      const answer = part2(input);
-      const { duration } = performance.measure("B", "B");
-      console.log("Part 2:", answer, `(${duration.toFixed(2)}ms)`);
-    }
+    const run = (part: number) => {
+      performance.mark("start");
+      const answer = part === 1 ? part1(input) : part2(input);
+      performance.mark("end");
+      const { duration } = performance.measure("start", "end");
+      console.log(`Part ${part}: ${answer} (${duration.toFixed(2)}ms)`);
+    };
+
+    if (part1) run(1);
+    if (part2) run(2);
   } catch {
-    // Not yet implemented
-    break;
+    return new Error(`Day ${day} not yet implemented`);
+  }
+}
+
+const selectedDay = Deno.args[0];
+
+if (selectedDay) {
+  runChallenge(Number(selectedDay));
+} else {
+  for (let i = 1; i <= 25; i++) {
+    try {
+      runChallenge(i);
+    } catch {
+      // Not yet implemented
+      break;
+    }
   }
 }
